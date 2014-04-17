8bc731791705f1c3194be2a60a8f6e3f6396f05f
Đorđe <manoilov88@gmail.com>
2014-04-17 13:02:04 +0200

    fax 17.04.2014

--- Matrice/Matrice/Matrice.cpp
+++ Matrice/Matrice/Matrice.cpp
@@ -16,14 +16,11 @@ int ** multiple_combinations;
 
 void InsertCombinations(const vector<int>& v, int *index) {
   static int count = 0;
-  cout << "combination no " << (++count) << ": [ ";
   for (int i = 0; i < v.size(); ++i) 
   { 
 	  //cout << v[i]-1 << " "; 
 	  multiple_combinations[*index-1][i] = v[i]-1;
-	  cout << *index-1;
   }
-  cout << "] " << endl;
 }
 
 void MultipleCombinations(int offset, int k, vector<int> elements, vector<int> combination, int * index) {
@@ -57,7 +54,6 @@ unsigned long long factorial(long long n)
 
 int _tmain(int argc, wchar_t *argv[])
 {
-	
 	int k = 2;
 	int n;
 	int t;
@@ -70,6 +66,7 @@ int _tmain(int argc, wchar_t *argv[])
 	int matrixes;
 	long long num_of_comb;
 
+	//////////////////////////////////////Unos korisnika////////////////////////////////////////////
 	cout << "Insert number of matrices: ";
 	cin >> matrixes;
 	cout << endl;
@@ -117,16 +114,18 @@ int _tmain(int argc, wchar_t *argv[])
 	cout << "Logic (1 for binary): ";
 	cin >> logic;
 	cout << endl;
+
+	/////////////////////////////////Kraj unosa////////////////////////////////////////////
 	if(t==p)
 		num_of_comb = 1;
 	else
-		num_of_comb = Factorial(t)/(Factorial(p)*(Factorial(t-p)));
+		num_of_comb = Factorial(t)/(Factorial(p)*(Factorial(t-p)));  //broj kombincija, za ispitivanje kolona
 
    
 	
-   cout<<endl;
-   cout<<endl;
-   const clock_t begin_time = clock(); // pocetak racunanja vremena
+    cout<<endl;
+    cout<<endl;
+    const clock_t begin_time = clock(); // pocetak racunanja vremena
 	vector<int> elements;
 	vector<int> col_elements;
     vector<int> combination;
@@ -149,101 +148,87 @@ int _tmain(int argc, wchar_t *argv[])
 	for (int i = 0; i < comb; ++i) {
 	 multiple_combinations[i] = new int[2]; //za svaku vrstu pravimo kolonu
 	}
-	MultipleCombinations(0,2,col_elements, combination, &index);
+	MultipleCombinations(0,2,col_elements, combination, &index); // u multiple_combinations, sve kombinacije reda 2 od n elemenata bez ponavljanja
 
 	int tmp = 0;
 	while(tmp == 0 && p <=m)
 	{
 		if(t==p)
-		num_of_comb = 1;
-	else
-		num_of_comb = Factorial(t)/(Factorial(p)*(Factorial(t-p)));
+			num_of_comb = 1;
+		else
+			num_of_comb = Factorial(t)/(Factorial(p)*(Factorial(t-p)));
 
-	//Combinations(0, 4, col_elements, combination);
-	Matrix * mat = new Matrix(n,m, num_of_comb, p);
-	mat->CreateRandomMatrix(n,m, matrixes, logic+1); //generisemo n matrica reda n x m 
-	mat->index = 0;
-	mat->num_of_combinations = num_of_comb; // n = 6, r = 3
-	mat->Combinations(0,p,col_elements, combination, 0);
+		Matrix * mat = new Matrix(n,m, num_of_comb, p);
+		mat->CreateRandomMatrix(n,m, matrixes, logic+1); //generisemo n matrica reda n x m 
+		mat->index = 0;
+		mat->num_of_combinations = num_of_comb; // n = 6, r = 3
+		mat->Combinations(0,p,col_elements, combination, 0);
 
 
-	ofstream myfile;
-    myfile.open ("result.txt");
+		ofstream myfile;
+		myfile.open ("result.txt");
  
-	int pera;
-	
-/*	for(int i=0;i<;i++)  // loop 3 times for three lines
-	{
-		for(int j=0;j<p;j++)  // loop for the three elements on the line
-		{
-			cout<<multiple_combinations[i][j];  // display the current element out of the array
-			cout << " ";
-		}
-		cout<<endl;  // when the inner loop is done, go to a new line
-		 
-	}*/
-
-  for(int counter =0; counter < matrixes; counter ++)
-  {
-	  //ucitavanje niza iz fajla i smestanje u matricu
-	  mat->FillMatrix(counter);
-	  myfile << "Matrix: " << counter << " "<< endl <<endl;
-		for(int i=0;i<n;i++)  //
-		{
-			for(int j=0;j<m;j++)  // 
-			{
-				myfile<<mat->matrix[i][j];  // display the current element out of the array
-				myfile << " ";
-			}
-		 myfile<<endl;  // when the inner loop is done, go to a new line
-		}
-		int tmp2 =0;
-
-	  //uzimamo svaku kombinaciju kolona
-	  for(int i=0; i<mat->num_of_combinations; i++)
+	  for(int counter =0; counter < matrixes; counter ++)
 	  {
-		int ** rows = mat->getRows(mat->all_combinations[i],p);
-		
-		//kombinacije svih vrsta za izabrane kolone
-		mat->Combinations(0,k,elements,combination, rows, n, p);
-	
-		int * index = mat->all_combinations[i];
-		if(mat->are_eq == false)
-		{   
-			tmp2++;
-			myfile << "Index: [";
-			int brojac=0;
-			for(brojac;brojac<p-1;brojac++)  // loop 3 times for three lines
+		  //ucitavanje niza iz fajla i smestanje u matricu
+		  mat->FillMatrix(counter);
+		  myfile << "Matrix: " << counter << " "<< endl <<endl;
+			for(int i=0;i<n;i++)  //
 			{
-				myfile <<index [brojac]<< ", ";
-			}
-			myfile << index [brojac] << "]"<< endl;
-			myfile << "Rows: " <<endl <<endl;
-			
-			for(int i=0;i<n;i++)  // loop 3 times for three lines
-			{
-				for(int j=0;j<p;j++)  // loop for the three elements on the line
+				for(int j=0;j<m;j++)  // 
 				{
-					myfile<<rows[i][j];  // display the current element out of the array
+					myfile<<mat->matrix[i][j];  // display the current element out of the array
 					myfile << " ";
 				}
 			 myfile<<endl;  // when the inner loop is done, go to a new line
-		 
 			}
-			myfile<<"-----------------------------------------------" << endl;
-		}
-		mat->are_eq = false;
+			int tmp2 =0;
+
+		  //uzimamo svaku kombinaciju kolona
+		  for(int i=0; i<mat->num_of_combinations; i++)
+		  {
+			int ** rows = mat->getRows(mat->all_combinations[i],p);
 		
-	  }
-	  if(tmp2>0)
-		  tmp ++;
-	  myfile<<"___________ END ______________" << endl;
-    }
-    myfile.close();
-	cout << "Number of solutions: " << tmp << endl;
-	cout << "Number of bits for coding: " << p << endl;
-	cout << "_____________________________" << endl;
-	p++;
+			//kombinacije svih vrsta za izabrane kolone
+			mat->Combinations(0,k,elements,combination, rows, n, p);
+	
+			int * index = mat->all_combinations[i];
+			if(mat->are_eq == false)
+			{   
+				tmp2++;
+				myfile << "Index: [";
+				int brojac=0;
+				for(brojac;brojac<p-1;brojac++)  // loop 3 times for three lines
+				{
+					myfile <<index [brojac]<< ", ";
+				}
+				myfile << index [brojac] << "]"<< endl;
+				myfile << "Rows: " <<endl <<endl;
+			
+				for(int i=0;i<n;i++)  // loop 3 times for three lines
+				{
+					for(int j=0;j<p;j++)  // loop for the three elements on the line
+					{
+						myfile<<rows[i][j];  // display the current element out of the array
+						myfile << " ";
+					}
+				 myfile<<endl;  // when the inner loop is done, go to a new line
+		 
+				}
+				myfile<<"-----------------------------------------------" << endl;
+			}
+			mat->are_eq = false;
+		
+		  }
+		  if(tmp2>0)
+			  tmp ++;
+		  myfile<<"___________ END ______________" << endl;
+		}
+		myfile.close();
+		cout << "Number of solutions: " << tmp << endl;
+		cout << "Number of bits for coding: " << p << endl;
+		cout << "_____________________________" << endl;
+		p++;
 	}
   cout << "Required time: " << float( clock () - begin_time ) /  CLOCKS_PER_SEC <<"s" <<endl;
 
